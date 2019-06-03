ea3e7f108d80981e01b994856d8eb082af87f265
Akash <47401212+CapClumsy@users.noreply.github.com>
2019-06-03 14:05:16 -0400

    Finished "Ms. Clarke's Thought of the Day" Command
    And made some style changes & shiz.

--- SMS_Bot.py
+++ SMS_Bot.py
@@ -1,12 +1,16 @@
 import discord
+import random
+
 from discord.ext import commands
 
 with open('TOKEN.txt', 'r') as f:
     TOKEN = f.readline()
 
-description = '''SMS Bot'''
+DESCRIPTION = '''SMS Bot'''
 
 UNHOLY_WORDS = ['airpods', 'juul', ' hat', 'leg', 'macbook', 'weed', 'phone', 'mitzvah', 'kazoo', 'sandals', 'spoon', 'amos', 'heck', 'hell', 'fork', 'wtf', 'fuck', 'shit', 'ass', 'dick', 'bitch']
+
+QUOTES = 
     
 bot = commands.Bot(command_prefix='s!', description=description)
 
@@ -24,6 +28,16 @@ async def test(ctx):
 @bot.command(name = 'supervan', description = "It's a bird...")
 async def supervan(ctx):
     await ctx.send("It's a bird. It's a plane. No... It's SuperVan!\nhttps://vimeo.com/83319818")
+    
+@bot.command(name = 'thought', description = "Ms. Clarke's thought of the day")
+async def thought(ctx):
+    choicepicked = random.randint(0, len(QUOTES) - 1)
+    i = 0
+    for x in QUOTES:
+        if choicepicked == i:
+            await ctx.send(x)
+            break
+        i += 1
 
 @bot.event
 async def on_message(message):                                                                        
@@ -32,8 +46,5 @@ async def on_message(message):
     if any([w in message.content.lower() for w in UNHOLY_WORDS  ]):
         await message.channel.send('Woah there buddy! Those words are infernal! You must change your ways and correct your sins. Go here to correct your errors, or the holy beings will disregard you for eternity.\nhttps://docs.google.com/document/d/10x45DFUNA1U0pE8ZYKLO64w_4y6fNGFH9B6M6tlMU3Q/edit')
     await bot.process_commands(message)
-    
-@bot.command(name = 'thought', description = "Ms. Clarks thought of the day")
-
                                
 bot.run(TOKEN)
